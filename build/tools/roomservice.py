AlphaDroid-Project/vendor_alpha

commit df70f4f68ab63549e8afdde679f4f108b6e0ada9

roomservice: check all device repositories
Some devices have the same codename but shipped a long time
ago and may not have a current branch set up.
Continue looking up all repositories until a match is found
or no repos are left to check.

For example:

  - android_device_amazon_thor
    - cm-14.1
  - android_device_xiaomi_thor
    - lineage-21.0

  - android_device_semc_zeus
    - jellybean
  - android_device_xiaomi_zeus
    - lineage-21.0

  - android_device_amazon_otter
    - cm-12.1
  - android_device_shift_otter
    - lineage-21.0

Change-Id: Icf9826b31efd872363fdfe73d5a71ff2d06cbe20
Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

build/tools/roomservice.py
@@ -282,7 +282,7 @@ def get_default_or_fallback_revision(repo_name):
     for branch in [branch['name'] for branch in result]:
         print(branch)
     print("Use the ROOMSERVICE_BRANCHES environment variable to specify a list of fallback branches.")
-    sys.exit()
+    return ""
 
 if depsonly:
     repo_path = get_from_manifest(device)
@@ -301,6 +301,12 @@ else:
             manufacturer = repo_name.replace("android_device_", "").replace("_" + device, "")
             repo_path = "device/%s/%s" % (manufacturer, device)
             revision = get_default_or_fallback_revision(repo_name)
+            if revision == "":
+                # Some devices have the same codename but shipped a long time ago and may not have
+                # a current branch set up.
+                # Continue looking up all repositories until a match is found or no repos are left
+                # to check.
+                continue
 
             device_repository = {'repository':repo_name,'target_path':repo_path,'branch':revision}
             add_to_manifest([device_repository])

commit bafcffc2a1a8c145ffa7c25f2603516a1447a4a4

roomservice: Exit if get_default_or_fallback_revision() fails
This was accidentally broken in df70f4f6.

Change-Id: I86c986202afb30cba14b1a1378f8403e462d0e55

build/tools/roomservice.py
@@ -233,6 +233,8 @@ def fetch_dependencies(repo_path):
                 if 'branch' not in dependency:
                     if dependency.get('remote', 'github') == 'github':
                         dependency['branch'] = get_default_or_fallback_revision(dependency['repository'])
+                        if not dependency['branch']:
+                            sys.exit(1)
                     else:
                         dependency['branch'] = None
             verify_repos.append(dependency['target_path'])